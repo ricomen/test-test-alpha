01019eaa121e977ce4040ede810b181631b5eb64
User <user@example.com>
2025-11-14 14:03:26 +0500

    Fix Store

--- store/useProductStore.ts
+++ store/useProductStore.ts
@@ -3,65 +3,76 @@ import type { IProduct } from '@/types'
 import { createJSONStorage, devtools, persist } from 'zustand/middleware'
 
 interface IInitialState {
-    products: IProduct[]
-    isLoading: boolean
+  products: IProduct[]
 }
 
 interface IProductActions {
-    fetchProducts: () => Promise<void>
-    toggleBookmark: (id: IProduct['id']) => void
-    addProduct: (product: IProduct) => void
-    removeProduct: (id: IProduct['id']) => void
+  toggleBookmark: (id: IProduct['id']) => void
+  addProduct: (product: IProduct) => void
+  removeProduct: (id: IProduct['id']) => void
+  getProduct: (id: IProduct['id']) => IProduct | undefined
+  setProducts: (products: IProduct[]) => void
 }
 
-interface IStoreState extends IInitialState, IProductActions { }
+interface IStoreState extends IInitialState, IProductActions {}
 
 const initialState: IInitialState = {
-    products: [],
-    isLoading: false,
+  products: [],
 }
 
-const counterStore: StateCreator<IStoreState, [["zustand/devtools", never], ["zustand/persist", unknown]]> = ((set, get) => ({
-    ...initialState,
-    toggleBookmark: (id: IProduct['id']) => set((state: IStoreState) => ({
-        products: state.products.map(product => ({
-            ...product,
-            ...(product.id === id && { isBookmarked: !product.isBookmarked })
-        }))
-    }), false, 'toggleBookmark'),
-    fetchProducts: async () => {
-        const state = get();
-        if (state.products.length > 0) {
-            return;
-        }
-        set({ isLoading: true }, false, 'fetchProducts');
-        try {
-            const response = await fetch('https://dummyjson.com/products');
-            const data = await response.json().then(data => data.products);
-            set({ products: data, isLoading: false }, false, 'fetchProducts/success');
-        } catch (error) {
-            console.error('Error fetching products:', error);
-            set({ products: [], isLoading: false }, false, 'fetchProducts/failed');
-        } finally {
-            set({ isLoading: false }, false, 'fetchProducts/finally');
-        }
-    },
-    addProduct: (product: IProduct) => set((state: IStoreState) => ({ products: [...state.products, product] }), false, "addProduct"),
-    removeProduct: (id: IProduct['id']) => set((state: IStoreState) => ({ products: state.products.filter((p) => p.id !== id) }), false, "removeProduct"),
-}))
+const counterStore: StateCreator<
+  IStoreState,
+  [['zustand/devtools', never], ['zustand/persist', unknown]]
+> = (set, get) => ({
+  ...initialState,
+  toggleBookmark: (id: IProduct['id']) =>
+    set(
+      (state: IStoreState) => ({
+        products: state.products.map((product) => ({
+          ...product,
+          ...(product.id === id && { isBookmarked: !product.isBookmarked }),
+        })),
+      }),
+      false,
+      'toggleBookmark'
+    ),
+  addProduct: (product: IProduct) =>
+    set(
+      (state: IStoreState) => ({ products: [...state.products, product] }),
+      false,
+      'addProduct'
+    ),
+  removeProduct: (id: IProduct['id']) =>
+    set(
+      (state: IStoreState) => ({
+        products: state.products.filter((p) => p.id !== id),
+      }),
+      false,
+      'removeProduct'
+    ),
+  getProduct: (id: IProduct['id']) => {
+    const state = get()
+    return state.products.find((p) => p.id === Number(id))
+  },
+  setProducts: (products: IProduct[]) => set(
+    () => ({ products: [ ...products] }),
+    false,
+    'setProducts'
+  ),
+})
 
 const useProductStore = create<IStoreState>()(
-    devtools(
-        persist(counterStore, {
-            name: "products-storage",
-            storage: createJSONStorage(() => localStorage)
-        })
-    )
+  devtools(
+    persist(counterStore, {
+      name: 'products-storage',
+      storage: createJSONStorage(() => localStorage),
+    })
+  )
 )
 
 export const useProducts = () => useProductStore(state => state.products)
-export const useIsLoading = () => useProductStore(state => state.isLoading)
-export const useFetchProducts = () => useProductStore(state => state.fetchProducts)
 export const useToggleBookmark = () => useProductStore(state => state.toggleBookmark)
 export const useRemoveProduct = () => useProductStore(state => state.removeProduct)
 export const useAddProduct = () => useProductStore(state => state.addProduct)
+export const useGetProduct = (id: IProduct['id']) => useProductStore(state => state.getProduct(id))
+export const useSetProducts = () => useProductStore(state => state.setProducts)
